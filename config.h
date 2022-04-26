52b78e02e012719f3728fd619439b50532fdb67d
fredins <fredin.martin@gmail.com>
2022-04-26 08:25:04 +0200

    Use light theme

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "JetBrainsMono:size=10:style=bold:antialias=true:autohint=true"
+    "JetBrainsMono:size=20:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char col_gray1[]       = "#1e2127";
@@ -14,9 +14,9 @@ static const char col_gray4[]       = "#e5e9f0";
 static const char col_pink[]        = "#b48ead";
 static const char *colors[SchemeLast][2] = {
 	             /*     fg         bg       */
-	[SchemeNorm] = { col_gray3, col_gray1 },
-	[SchemeSel] = { col_gray3, col_pink },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = {col_gray1, "#edeceb"},
+	[SchemeSel] = { col_gray1, col_pink },
+	[SchemeOut] = { "#edeceb", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
